d5bd68caf89300d9f2c72676bd80de6990a6c80a
Nishant Minerva <nishantcafe@gmail.com>
2025-05-21 13:05:24 +0530

    Initial Commit

---
+++ AnswersAi/AnswersAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnswersAiApp.swift
+//  AnswersAi
+//
+//  Created by Nishant on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AnswersAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
